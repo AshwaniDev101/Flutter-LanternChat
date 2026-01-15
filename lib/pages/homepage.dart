6458017af506a8abbaed362f19bcd79714ec54e2
AshwaniDev101 <54770264+Ashwani10101@users.noreply.github.com>
2026-01-15 19:11:47 +0530

    feat: Implement app bar and menu for homepage
    This commit introduces a new app bar to the homepage, which includes icons for QR code scanning and camera access.
    
    A popup menu has been added with the following options:
    - New Group
    - New Community
    - Broadcast list
    - Linked Devices
    - Starred
    - Payments
    - Read all
    - Settings
    
    The "Linked Devices" menu item has an attention indicator. The underlying logic for handling menu item selections has also been implemented.

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,23 +1,96 @@
-
 import 'package:flutter/material.dart';
+
+enum _HomepageMenuActions { newGroup, newCommunity, broadcastList, linkedDevices, starred, payments, readAll, settings }
+
+extension on _HomepageMenuActions {
+  String get label {
+    return switch (this) {
+      _HomepageMenuActions.newGroup => 'New Group',
+      _HomepageMenuActions.newCommunity => 'New Community',
+      _HomepageMenuActions.broadcastList => 'Broadcast list',
+      _HomepageMenuActions.linkedDevices => 'Linked Devices',
+      _HomepageMenuActions.starred => 'Starred',
+      _HomepageMenuActions.payments => 'Payments',
+      _HomepageMenuActions.readAll => 'Read all',
+      _HomepageMenuActions.settings => 'Settings',
+    };
+  }
+
+  bool get isAttention {
+    return _handleAttention(this);
+  }
+}
+
+bool _handleAttention(_HomepageMenuActions action) {
+  if (action == _HomepageMenuActions.linkedDevices) {
+    return true;
+  }
+  return false;
+}
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(title: Text('Lantern Chat'),),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Text('Hello world'),
-          )
+      appBar: AppBar(
+        title: Text('LanternChat'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.qr_code_scanner_rounded)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.camera_alt_outlined)),
+          PopupMenuButton<_HomepageMenuActions>(
+            itemBuilder: (context) {
+              return _HomepageMenuActions.values.map((menuAction) {
+                return PopupMenuItem(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  value: menuAction,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.circle,
+                        size: 10,
+                        color: menuAction.isAttention ? Colors.amberAccent : Colors.transparent,
+                      ),
+                      SizedBox(width: 5),
+                      Text(menuAction.label),
+                    ],
+                  ),
+                );
+              }).toList();
+            },
+
+            onSelected: (value) {
+              _handleMenuAction(value);
+            },
+          ),
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Column(children: [Center(child: Text("Hello World!"))]),
+      ),
     );
+  }
+
+  void _handleMenuAction(_HomepageMenuActions value) {
+    switch (value) {
+      case _HomepageMenuActions.newGroup:
+        break;
+      case _HomepageMenuActions.newCommunity:
+        break;
+      case _HomepageMenuActions.broadcastList:
+        break;
+      case _HomepageMenuActions.linkedDevices:
+        break;
+      case _HomepageMenuActions.starred:
+        break;
+      case _HomepageMenuActions.payments:
+        break;
+      case _HomepageMenuActions.readAll:
+        break;
+      case _HomepageMenuActions.settings:
+        break;
+    }
   }
 }
