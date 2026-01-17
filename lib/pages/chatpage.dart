5fdc6cd0b4a2e8ccf78a48f1e46b57400b9a64db
AshwaniDev101 <ashwani.yadav.dev@gmail.com>
2026-01-17 18:56:40 +0530

    feat: Implement basic chat message bubble UI
    This commit replaces the empty body of the `Chatpage` with a list of chat message bubbles.

--- lib/pages/chatpage.dart
+++ lib/pages/chatpage.dart
@@ -11,11 +11,9 @@ enum ChatpagePopupMenu {
   more,
 }
 
-extension on ChatpagePopupMenu
-{
-  String get action{
-    return switch(this)
-    {
+extension on ChatpagePopupMenu {
+  String get action {
+    return switch (this) {
       ChatpagePopupMenu.newGroup => "New Group",
       ChatpagePopupMenu.viewContact => "View Contact",
       ChatpagePopupMenu.search => "Search",
@@ -41,14 +39,62 @@ class Chatpage extends StatelessWidget {
           IconButton(onPressed: () {}, icon: Icon(Icons.call_outlined)),
           PopupMenuButton(
             itemBuilder: (context) {
-              return ChatpagePopupMenu.values.map((value){
-                return PopupMenuItem(value:value, child: Text(value.action));
+              return ChatpagePopupMenu.values.map((value) {
+                return PopupMenuItem(value: value, child: Text(value.action));
               }).toList();
             },
           ),
         ],
       ),
-      body: Column(children: []),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+
+
+          final isSelf = index%2;
+
+          if(isSelf==0){
+            return Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Colors.blueGrey,
+                
+                    border: Border.all(color: Colors.white10),
+                  ),
+                
+                  child: Text('hi  how are you'),
+                ),
+
+                IconButton(onPressed: (){}, icon: Icon(Icons.subdirectory_arrow_right_outlined)),
+              ],
+            );
+          }else {
+            return Row(
+              children: [
+
+                IconButton(onPressed: (){}, icon: Icon(Icons.subdirectory_arrow_right_outlined)),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Colors.green,
+                
+                    border: Border.all(color: Colors.white10),
+                  ),
+                
+                  child: Text('hi  how are you'),
+                ),
+              ],
+            );
+
+          }
+
+
+        },
+      ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Row(
