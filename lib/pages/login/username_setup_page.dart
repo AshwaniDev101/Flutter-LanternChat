8fdce7da8ef439230f4f3e548ce075836c3e259d
AshwaniDev101 <ashwani.yadav.dev@gmail.com>
2026-02-16 09:51:13 +0530

    feat: Create initial UsernameSetupPage
    This commit introduces a new `UsernameSetupPage` to display user information after authentication. This page is intended for new users to set up their profiles.
    
    Key changes:
    - A new file, `lib/pages/login/username_setup_page.dart`, has been created.
    - The page is a `ConsumerWidget` that uses Riverpod to access the current authenticated user's data from `firebaseAuthProvider`.
    - It displays basic user details such as `displayName`, `email`, `phoneNumber`, `emailVerified`, creation time, and last sign-in time.
    - A fallback UI is included to show an error message if no user is found.
    - The page layout uses a `Scaffold` and `SafeArea`, with user information centered in a `Column`.

---
+++ lib/pages/login/username_setup_page.dart
@@ -0,0 +1,49 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lanternchat/core/providers/auth_provider.dart';
+
+class UsernameSetupPage extends ConsumerWidget {
+  const UsernameSetupPage({super.key});
+
+  @override
+  Widget build(BuildContext context, ref) {
+
+
+    final user = ref.watch(firebaseAuthProvider).currentUser;
+
+    if(user==null)
+      {
+        return Scaffold(
+          body: Center(
+            child: Text('UsernameSetupPage: Something went wrong 404'),
+          ),
+        );
+      }
+
+
+    return Scaffold(
+
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+
+
+              Text('${user.displayName}'),
+              Text('${user.email}'),
+              Text('${user.phoneNumber}'),
+              Text('${user.emailVerified}'),
+              Text('${user.metadata.creationTime}'),
+              Text('${user.metadata.lastSignInTime}'),
+
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+}
