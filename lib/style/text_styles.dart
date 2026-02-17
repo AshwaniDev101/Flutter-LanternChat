7044c95b7d594161190204cd99060fa521f3fbb9
AshwaniDev101 <ashwani.yadav.dev@gmail.com>
2026-02-17 09:52:35 +0530

    feat: Add initial text styles and update .gitignore
    This commit introduces a new file for managing text styles and updates the `.gitignore` file to exclude an additional directory and a specific file.
    
    Key changes:
    - **Text Styles (`lib/style/text_styles.dart`):**
        - A new file `text_styles.dart` has been created to centralize application text styling.
        - It includes a `TextStyles` class with a `textTheme` property and extensive comments detailing Material Design's default text styles and their typical use cases (e.g., Display, Headline, Title, Body, Label).
    
    - **Gitignore Updates:**
        - The path `/lib/archive/` has been simplified to `/archive/`.
        - The file `/archive/widget_sandbox.dart` has been added to the ignore list.

---
+++ lib/style/text_styles.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+
+class TextStyles
+{
+  // TextStyles
+  // Display   → Huge text (hero / splash / marketing)
+  // Headline  → Big section headers
+  // Title     → Card / dialog titles
+  // Body      → Normal readable text
+  // Label     → Buttons, small UI labels
+
+
+  // === Default text Styles ===
+  // Style Font    Size	Weight	Typical Use
+  // displayLarge  	57	400	    Hero
+  // displayMedium	45	400	    Big banner
+  // displaySmall  	36	400	    Large heading
+  // headlineLarge	32	400	    Screen title
+  // headlineMedium	28	400	    Section header
+  // headlineSmall	24	400	    Smaller header
+  // titleLarge	    22	400	    Card/Dialog title
+  // titleMedium	  16	500	    Medium emphasis
+  // titleSmall   	14	500	    Small component title
+  // bodyLarge	    16	400	    Primary text
+  // bodyMedium   	14	400	    Default text
+  // bodySmall      12	400	    Secondary text
+  // labelLarge    	14	500	    Button text
+  // labelMedium	  12	500	    Tabs/Chips
+  // labelSmall	    11	500	    Tiny labels
+  
+  static const TextTheme textTheme = TextTheme(
+
+  );
+}
